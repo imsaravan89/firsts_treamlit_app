a98ddfaf7f42ea77752df9e33e741e213408259e
imsaravan89 <96118428+imsaravan89@users.noreply.github.com>
2022-11-05 13:15:12 -0400

    Update streamlit_app.py
    include pandas and create fruitlist dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healty Diner')  
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinace & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruit_list)
