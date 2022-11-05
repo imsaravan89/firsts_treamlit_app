448328d3670e520fb502762607edc0af46be797b
imsaravan89 <96118428+imsaravan89@users.noreply.github.com>
2022-11-05 13:23:05 -0400

    Update streamlit_app.py
    fruits selected and show

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_list)
 
 
